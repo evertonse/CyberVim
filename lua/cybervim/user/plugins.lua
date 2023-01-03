09e5e5cabdc7a2d597a9e7a72667e62eeda15924
Everton Jr <evertonse.junior@gmail.com>
2023-01-03 14:26:34 -0300

    clenanup packer file for understanding

--- lua/cybervim/user/plugins.lua
+++ lua/cybervim/user/plugins.lua
@@ -1,6 +1,6 @@
 local fn = vim.fn
 
--- Automatically install packer
+-->> Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
 	PACKER_BOOTSTRAP = fn.system({
@@ -15,7 +15,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
 	vim.cmd[[packadd packer.nvim]]
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
+-->> Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
   augroup packer_user_config
     autocmd!
@@ -23,13 +23,13 @@ vim.cmd([[
   augroup end
 ]])
 
--- Use a protected call so we don't error out on first use
+-->> Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
 	return
 end
 
--- Have packer use a popup window
+-->> Have packer use a popup window
 packer.init({
 	display = {
 		open_fn = function()
@@ -38,7 +38,7 @@ packer.init({
 	},
 })
 
--- Install your plugins here
+-->> Install your plugins here
 return packer.startup(function(use)
   use { "wbthomason/packer.nvim", commit = "6afb67460283f0e990d35d229fd38fdc04063e0a" } -- Have packer manage itself
   use { "nvim-lua/plenary.nvim", commit = "4b7e52044bbb84242158d977a50c4cbcd85070c7" } -- Useful lua functions used by lots of plugins
@@ -58,11 +58,7 @@ return packer.startup(function(use)
   use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
 	use {"folke/which-key.nvim"}
 
-	-- Colorschemes
-  use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
-  use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
-
-	-- Cmp 
+	-->> Cmp 
   use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
   use { "hrsh7th/cmp-buffer", commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" } -- buffer completions
   use { "hrsh7th/cmp-path", commit = "447c87cdd6e6d6a1d2488b1d43108bfa217f56e1" } -- path completions
@@ -70,10 +66,10 @@ return packer.startup(function(use)
 	use { "hrsh7th/cmp-nvim-lsp", commit = "3cf38d9c957e95c397b66f91967758b31be4abe6" }
 	use { "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" }
 
-	-- Snippets
+	-->> Snippets
   use { "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" } --snippet engine
   use { "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
-	-- LSP
+	-->> LSP
 
 	use { "neovim/nvim-lspconfig", commit = "f11fdff7e8b5b415e5ef1837bdcdd37ea6764dda" } -- enable LSP
   use { "williamboman/mason.nvim", commit = "c2002d7a6b5a72ba02388548cfaf420b864fbc12"} -- simple to use language server installer
@@ -81,10 +77,10 @@ return packer.startup(function(use)
 	use { "jose-elias-alvarez/null-ls.nvim", commit = "c0c19f32b614b3921e17886c541c13a72748d450" } -- for formatters and linters
   use { "RRethy/vim-illuminate", commit = "a2e8476af3f3e993bb0d6477438aad3096512e42" }
 
-	-- Telescope
+	-->> Telescope
 	use { "nvim-telescope/telescope.nvim", commit = "76ea9a898d3307244dce3573392dcf2cc38f340f" }
 
-	-- Treesitter
+	-->> Treesitter
 	use {
 		"nvim-treesitter/nvim-treesitter",
     commit = "12e95e160d7d45b76a36bca6303dd8447ab77490" -- Commits on Jan 2, 2023
@@ -92,22 +88,26 @@ return packer.startup(function(use)
     --commit = "256802258084fcf6c7011dae4c3fbfaaf4b61518" -- Commits on Dec 1, 2022
 	}
   use {'nvim-treesitter/playground'}
-	-- Git
+	-->> Git
 	use { "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
 
+	-->> Colorschemes
+  use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
+  use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
   --use 'tomasiser/vim-code-dark'
   use 'dstein64/vim-startuptime'
   use 'shaunsingh/nord.nvim'
   -- Another vs code theme:
   -- for more options see: https://github.com/Mofiqul/vscode.nvim
   --use 'Mofiqul/vscode.nvim'
-  
+
   -- Using my fork of Mofiqul vscode nvim theme, 
   -- but my theme is Focusing on Visual Studio Theme, rather tha vs code
   use {'evertonse/vs.nvim' , branch = "dev"}
   --use 'marko-cerovac/material.nvim'
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+
 	use { 'm-demare/hlargs.nvim',commit = "88b925d699fb39633cdda02c24f0b3ba5d0e6964", requires = { 'nvim-treesitter/nvim-treesitter' } }
   
   if PACKER_BOOTSTRAP then
