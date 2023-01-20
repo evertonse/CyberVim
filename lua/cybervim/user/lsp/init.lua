b37b4ee8ee8d46a91a073259d09a4c460544561a
Everton Jr <evertonse.junior@gmail.com>
2023-01-20 18:28:37 -0300

    lsp kinda icos + refactor lsp

--- lua/cybervim/user/lsp/init.lua
+++ lua/cybervim/user/lsp/init.lua
@@ -1,5 +1,6 @@
 
 --require("cybervim.user.lsp.handlers").setup()
+require "cybervim.user.lsp.mason"
 require "cybervim.user.lsp.lsp_zero"
 require "cybervim.user.lsp.lsp_kind"
 require "cybervim.user.lsp.null-ls"
--- lua/cybervim/user/lsp/mason.lua
+++ lua/cybervim/user/lsp/mason.lua
@@ -26,31 +26,4 @@ local settings = {
 	max_concurrent_installers = 1,
 }
 
-require("mason").setup(settings)
-require("mason-lspconfig").setup({
-	ensure_installed = servers,
-	automatic_installation = true,
-})
-
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status_ok then
-	return
-end
-
-local opts = {}
-
-for _, server in pairs(servers) do
-	opts = {
-		on_attach = require("cybervim.user.lsp.handlers").on_attach,
-		capabilities = require("cybervim.user.lsp.handlers").capabilities,
-	}
-
-	server = vim.split(server, "@")[1]
-
-	local require_ok, conf_opts = pcall(require, "cybervim.user.lsp.settings." .. server)
-	if require_ok then
-		opts = vim.tbl_deep_extend("force", conf_opts, opts)
-	end
-
-	lspconfig[server].setup(opts)
-end
+require("mason.settings").set(settings)
